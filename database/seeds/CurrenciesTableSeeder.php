669ae39dcdfc3c2d49110d2ed3ac13bbf1bfa340
Wout Peeters <wout.peeters@immalle.be>
2017-05-09 18:41:36 +0200

    Seeder voor currencies aangemaakt

---
+++ database/seeds/CurrenciesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CurrenciesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('currencies')->insert([
+            'valuta' => "€",
+            'naam' => "euro",
+            'afkorting' => "EUR",
+            'bedrag' => 1,
+        ]);
+        
+        DB::table('currencies')->insert([
+            'valuta' => "$",
+            'naam' => "dollar",
+            'afkorting' => "USD",
+            'bedrag' => 1.087695,
+        ]);
+        
+    }
+}
